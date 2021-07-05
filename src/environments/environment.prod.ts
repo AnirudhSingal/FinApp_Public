bdf34aed219f36616f8b3abda7291bd7fd98b232
AnirudhSingal <Singal.anirudh.1991@gmail.com>
2021-07-04 20:07:00 -0400

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  authUrl : 'https://localhost:5000/',
+  FinDataApi : 'https://financialmodelingprep.com/api/v3/',
+  FinDataApiKey : undefined,
+};
